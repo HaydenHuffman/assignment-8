cecdb8831e0c382a1795a608c045258310ab69e1
HaydenHuffman <120144719+HaydenHuffman@users.noreply.github.com>
2023-08-29 11:28:43 -0400

    Initial commit

---
+++ src/com/coderscampus/assignment8/Main.java
@@ -0,0 +1,38 @@
+package com.coderscampus.assignment8;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) {
+		
+		
+		Assignment8 assignment8 = new Assignment8();
+		 
+	
+		
+		ExecutorService pool = Executors.newCachedThreadPool();
+	
+		List<List<Integer>> numbers = new ArrayList<>();
+		
+		for(int i = 0; i < 1000; i++) {
+		
+	            CompletableFuture<Void> number = CompletableFuture.runAsync(() -> {
+	                List<Integer> numbersList = Collections.synchronizedList(assignment8.getNumbers());}, pool);
+		
+				}
+		
+		
+	
+		}
+	
+	}
+			
+		
+		
